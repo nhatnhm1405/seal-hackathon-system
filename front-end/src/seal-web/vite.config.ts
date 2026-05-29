1fa230b1dd788e9eb0471410224deb8bfdd3f14e
KhanhNLH <nguyenlekhanh2608@gmail.com>
2026-05-29 10:49:04 +0700

    feat: add TailwindCSS v4 and add Custom Figma asset resolver plugin

--- front-end/src/seal-web/vite.config.ts
+++ front-end/src/seal-web/vite.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type Plugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+
+plugins: [react(), tailwindcss()]
 
 // https://vite.dev/config/
+
+function figmaAssetResolver(): Plugin {
+  return {
+    name: 'figma-asset-resolver',
+    resolveId(id) {
+      if (id.startsWith('figma:asset/')) {
+        return path.resolve(__dirname, 'src/assets', id.replace('figma:asset/', ''))
+      }
+    }
+  }
+}
+
+plugins: [figmaAssetResolver(), react(), tailwindcss()]
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/app')
