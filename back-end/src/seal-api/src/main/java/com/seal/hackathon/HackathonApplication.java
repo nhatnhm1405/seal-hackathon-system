1189eedd34f918cfca893a0b2243ce7a20afd9fb
Đào Hoàng Nhật <daonhat082@gmail.com>
2026-06-15 08:49:11 +0700

    Update API configuration

--- back-end/src/seal-api/src/main/java/com/seal/hackathon/HackathonApplication.java
+++ back-end/src/seal-api/src/main/java/com/seal/hackathon/HackathonApplication.java
@@ -8,7 +8,7 @@ public class HackathonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HackathonApplication.class, args);
-
+		System.out.println("Run successfully");
 
 	}
 }
